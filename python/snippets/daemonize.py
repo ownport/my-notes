6682a6cbd3f0030fcba25f5499595fd430e896d4
Andrey Usov <ownport@gmail.com>
2012-08-14 05:25:30 +0300

    added few snippets

---
+++ python/snippets/daemonize.py
@@ -0,0 +1,43 @@
+# extracted from ginkgo 
+# https://github.com/progrium/ginkgo/blob/master/ginkgo/util.py
+#
+def daemonize(preserve_fds=None):
+    """\
+    Standard daemonization of a process.
+    http://www.svbug.com/documentation/comp.unix.programmer-FAQ/faq_2.html#SEC16
+    """
+    def _maxfd(limit=1024):
+        maxfd = resource.getrlimit(resource.RLIMIT_NOFILE)[1]
+        if maxfd == resource.RLIM_INFINITY:
+            return limit
+        else:
+            return maxfd
+
+    def _devnull(default="/dev/null"):
+        if hasattr(os, "devnull"):
+            return os.devnull
+        else:
+            return default
+
+    def _close_fds(preserve=None):
+        preserve = preserve or []
+        for fd in xrange(0, _maxfd()):
+            if fd not in preserve:
+                try:
+                    os.close(fd)
+                except OSError: # fd wasn't open to begin with (ignored)
+                    pass
+
+    if os.fork():
+        os._exit(0)
+    os.setsid()
+
+    if os.fork():
+        os._exit(0)
+
+    os.umask(0)
+    _close_fds(preserve_fds)
+
+    os.open(_devnull(), os.O_RDWR)
+    os.dup2(0, 1)
+    os.dup2(0, 2)
